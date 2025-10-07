8f346f82aab74552269d05042ce2025074278d66
niitroxendioxide <niitroxendioxide@gmail.com>
2025-10-06 22:07:26 -0300

    events-router && events-service. need work to do

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(' ')[1]
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' })
+    }
+
+    console.log("Token: ", token);
+
+    next();
+}
