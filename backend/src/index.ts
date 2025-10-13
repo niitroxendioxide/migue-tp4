d148b9e966fd11c7bf66923190f3daf1249ecacf
niitroxendioxide <niitroxendioxide@gmail.com>
2025-10-13 20:49:36 -0300

    added routers for payment & events  +  added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,25 @@
 import express from 'express'
+import cors from "cors";
+
 import { eventsRouter } from './routers/events-router'
 import { errorHandlerMiddleware } from './middleware/errors'
 import { registerRouter } from './routers/register-router'
+import { paymentRouter } from './routers/payment-router'
 
 const app = express()
 
 app.use(express.json())
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+		credentials: true, 
+		optionsSuccessStatus: 200, 
+	}),
+);
+
+app.use("/payment", paymentRouter);
 app.use("/api", registerRouter);
 app.use('/events', eventsRouter);
 app.get('/health', (req, res) => {
---
+++ backend/src/types/express.d.ts
@@ -0,0 +1,9 @@
+// src/types/express.d.ts
+declare namespace Express {
+  export interface Request {
+    user: {
+      id: number;
+      token: string;
+    }
+  }
+}
